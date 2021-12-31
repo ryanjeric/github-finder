7fab16d79bb0afd0074f58befe0db70359b89d7f
ryanjericsabado <ryanjericsabado@yahoo.com>
2022-01-01 00:54:07 +0800

    INIT

---
+++ src/components/layouts/Spinner.jsx
@@ -0,0 +1,16 @@
+import SpinnerGif from "./assets/spinner.gif";
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        src={SpinnerGif}
+        alt="Loading"
+        width={180}
+        className="text-center mx-auto"
+      />
+    </div>
+  );
+}
+
+export default Spinner;
